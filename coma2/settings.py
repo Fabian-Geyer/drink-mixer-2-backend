8f20a936517d7c7f5ffb897616cefe1ce84f7d8f
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-07 16:10:11 +0200

    change to SQLAlchemy db structure

--- coma2/settings.py
+++ coma2/settings.py
@@ -1,17 +1,14 @@
 import os
 from flask import Flask
 from dotenv import find_dotenv, load_dotenv
-from flaskext.mysql import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 # constants
 DATABASE_URI = os.environ.get("DATABASE_PATH")
 
 
-# init app
+# init Flask app and SQLAlchemy database object
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# MySQL init
-db = MySQL()
-db.init_app(app)
+db = SQLAlchemy(app)
