53a7283eadc7a469b1b9b18d979162261b4f6a51
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-08 20:23:59 +0200

    check for duplicates when adding ingredients

--- coma2/main.py
+++ coma2/main.py
@@ -1,5 +1,4 @@
-import flask
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from coma2.models.ingredients import Ingredients
 
 from coma2.settings import app, db
@@ -9,13 +8,19 @@ import coma2.constants as c
 @app.route("/api/ingredients", methods=["POST", "GET"])
 def handle_ingredient():
     if request.method == "POST":
-        name = request.get_json()["name"]
-        alcohol_percentage = request.get_json()["alcohol_percentage"]
-        ingredient = Ingredients(
-            name=name, alcohol_percentage=alcohol_percentage)
-        db.session.add(ingredient)
-        db.session.commit()
-        return jsonify(ingredient.serialize)
+        ingred_name = request.get_json()["name"]
+        # check for duplicates
+        duplicate = Ingredients.query.filter_by(name=ingred_name).first()
+        print("duplicate:: " + str(duplicate))
+        if not duplicate:
+            alcohol_percentage = request.get_json()["alcohol_percentage"]
+            ingredient = Ingredients(
+                name=ingred_name, alcohol_percentage=alcohol_percentage)
+            db.session.add(ingredient)
+            db.session.commit()
+            return jsonify(ingredient.serialize)
+        else:
+            return abort(400, "This Ingredient name already exists")
     if request.method == "GET":
         return jsonify([elem.serialize for elem in Ingredients.query.all().order_by()])
         #TODO: allow filtering by alcohol etc.
