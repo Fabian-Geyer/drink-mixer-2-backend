db77d8616449f631defaa8ac4d5e672babc29b48
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-08 20:52:59 +0200

    update gitignore

--- coma2/main.py
+++ coma2/main.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request, abort
-from coma2.models.ingredients import Ingredients
+from coma2.models.cocktails import Ingredient
 
 from coma2.settings import app, db
 import coma2.constants as c
@@ -10,11 +10,11 @@ def handle_ingredient():
     if request.method == "POST":
         ingred_name = request.get_json()["name"]
         # check for duplicates
-        duplicate = Ingredients.query.filter_by(name=ingred_name).first()
+        duplicate = Ingredient.query.filter_by(name=ingred_name).first()
         print("duplicate:: " + str(duplicate))
         if not duplicate:
             alcohol_percentage = request.get_json()["alcohol_percentage"]
-            ingredient = Ingredients(
+            ingredient = Ingredient(
                 name=ingred_name, alcohol_percentage=alcohol_percentage)
             db.session.add(ingredient)
             db.session.commit()
@@ -22,8 +22,8 @@ def handle_ingredient():
         else:
             return abort(400, "This Ingredient name already exists")
     if request.method == "GET":
-        return jsonify([elem.serialize for elem in Ingredients.query.all().order_by()])
-        #TODO: allow filtering by alcohol etc.
+        return jsonify([elem.serialize for elem in Ingredient.query.all().order_by()])
+        # TODO: allow filtering by alcohol etc.
 
     if __name__ == "__main__":
         app.run(c.DEBUG)
