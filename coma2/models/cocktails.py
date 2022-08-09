60c2a4eee4fc84392f92d44e1dae8bf09b761e5e
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-09 14:57:37 +0200

    update db structure for amount

--- coma2/models/cocktails.py
+++ coma2/models/cocktails.py
@@ -3,12 +3,58 @@ from datetime import datetime
 from coma2.settings import db
 
 
-cocktail_ingredient = db.Table('cocktail_ingredient',
-                               db.Column('cocktail_id', db.Integer,
-                                         db.ForeignKey('cocktail.id')),
-                               db.Column('ingredient_id', db.Integer,
-                                         db.ForeignKey('ingredient.id'))
-                               )
+# cocktail_ingredient = db.Table('cocktail_ingredient',
+#                               db.Column('cocktail_id', db.Integer,
+#                                         db.ForeignKey('cocktail.id')),
+#                               db.Column('ingredient_id', db.Integer,
+#                                         db.ForeignKey('ingredient.id'))
+#                               )
+
+class CocktailIngredient(db.Model):
+    __tablename__ = "cocktail_ingredient"
+    cocktail_id = db.Column(db.ForeignKey("cocktail.id"), primary_key=True)
+    ingredient_id = db.Column(db.ForeignKey("ingredient.id"), primary_key=True)
+    amount = db.Column("amount", db.Integer)
+    ingredient = db.relationship("Ingredient", back_populates="cocktails")
+    cocktail = db.relationship("Cocktail", back_populates="ingredients")
+
+
+class Cocktail(db.Model):
+    """
+    Table to keep track of Cocktails and their names
+    """
+
+    __tablename__ = "cocktail"
+
+    id = db.Column("id",
+                   db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    timestamp = db.Column("timestamp",
+                          db.DateTime,
+                          nullable=False,
+                          default=datetime.utcnow)
+    name = db.Column("name",
+                     db.String)
+    ingredients = db.relationship(
+        "CocktailIngredient", back_populates="cocktail")
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"""{self.timestamp} {self.id} {self.name}"""
+
+    @property
+    def serialize(self):
+        """
+        Return item in serializeable format
+        """
+        return {"id": self.id,
+                "timestamp": self.timestamp,
+                "name": self.name,
+                "ingredients": [i.name for i in self.ingredients]
+                }
 
 
 class Ingredient(db.Model):
@@ -30,6 +76,8 @@ class Ingredient(db.Model):
                      db.String)
     alcohol_percentage = db.Column("alcohol_percentage",
                                    db.Integer)
+    cocktails = db.relationship(
+        "CocktailIngredient", back_populates="ingredient")
 
     def __init__(self, name, alcohol_percentage):
         self.name = name
@@ -46,42 +94,4 @@ class Ingredient(db.Model):
         return {"id": self.id,
                 "timestamp": self.timestamp,
                 "name": self.name,
-                }
-
-
-class Cocktail(db.Model):
-    """
-    Table to keep track of Cocktails and their names
-    """
-
-    __tablename__ = "cocktail"
-
-    id = db.Column("id",
-                   db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-    timestamp = db.Column("timestamp",
-                          db.DateTime,
-                          nullable=False,
-                          default=datetime.utcnow)
-    name = db.Column("name",
-                     db.String)
-    ingredients = db.relationship(
-        'Ingredient', secondary=cocktail_ingredient, backref='cocktails')
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return f"""{self.timestamp} {self.id} {self.name}"""
-
-    @property
-    def serialize(self):
-        """
-        Return item in serializeable format
-        """
-        return {"id": self.id,
-                "timestamp": self.timestamp,
-                "name": self.name,
-                "ingredients": [i.name for i in self.ingredients]
                 }
--- create_tables.py
+++ create_tables.py
@@ -1,22 +1,33 @@
-from coma2.models.cocktails import Ingredient, Cocktail
+from coma2.models.cocktails import Ingredient, Cocktail, CocktailIngredient
 from coma2.models.cocktails import db as cocktail_db
 
 cocktail_db.drop_all()
 cocktail_db.create_all()
 
+
 ingred_1 = Ingredient(name="Orangesaft", alcohol_percentage=0)
 ingred_2 = Ingredient(name="Wodka", alcohol_percentage=40)
 ingred_3 = Ingredient(name="Apfelsaft", alcohol_percentage=0)
 
+# Cocktail 1
 cocktail_1 = Cocktail(name="Screwdriver")
-cocktail_2 = Cocktail(name="Apfel-O")
+ci_1 = CocktailIngredient(amount=2)
+ci_1.ingredient = ingred_1
+cocktail_1.ingredients.append(ci_1)
+ci_2 = CocktailIngredient(amount=1)
+ci_2.ingredient = ingred_2
+cocktail_1.ingredients.append(ci_2)
 
-cocktail_1.ingredients.append(ingred_1)
-cocktail_1.ingredients.append(ingred_2)
-cocktail_2.ingredients.append(ingred_1)
-cocktail_2.ingredients.append(ingred_3)
+
+# Cocktail 2
+cocktail_2 = Cocktail(name="Apfel-O")
+ci_3 = CocktailIngredient(amount=1)
+ci_3.ingredient = ingred_1
+cocktail_2.ingredients.append(ci_3)
+ci_4 = CocktailIngredient(amount=1)
+ci_4.ingredient = ingred_3
+cocktail_2.ingredients.append(ci_4)
 
 cocktail_db.session.add_all([ingred_1, ingred_2, ingred_3])
-cocktail_db.session.add_all([cocktail_1, cocktail_2])
 
 cocktail_db.session.commit()
