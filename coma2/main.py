7f857d76cb255724631da6d08320fb8a27c0b0e9
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-11 16:34:50 +0200

    add PUT slots for changing slot data

--- coma2/main.py
+++ coma2/main.py
@@ -1,6 +1,8 @@
+from crypt import methods
 from flask import jsonify, request, abort
 
 from coma2.models.cocktails import Cocktail, CocktailIngredient, Ingredient
+from coma2.models.slots import Slot
 from coma2.settings import app, db
 import coma2.constants as c
 
@@ -52,6 +54,21 @@ def handle_cocktail():
         return jsonify([elem.serialize for elem in db.session.query(
             Cocktail).order_by(Cocktail.name)])
 
+
+@app.route("/api/slots", methods=["PUT"])
+def handle_slot():
+    """Endpoint to get info about slots and to change single slots"""
+    if request.method == "PUT":
+        req = request.get_json()
+        slot = Slot.query.filter_by(id=req["id"]).first()
+        if "ingredient_id" in req.keys():
+            slot.ingredient_id = req["ingredient_id"]
+        if "amount_percentage" in req.keys():
+            slot.amount_percentage = req["amount_percentage"]
+        db.session.commit()
+        return jsonify(slot.serialize)
+        
+        
 # TODO: add slot endpoint (optional: ingred_id)
 # TODO: delete slot by id endpoint
 # TODO: edit slot by id endpoint
