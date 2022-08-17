46679f15489006a92b67c2a5e20cdd517a789b34
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-17 22:11:35 +0200

    add CORS to allow all http requests

--- coma2/settings.py
+++ coma2/settings.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_cors import CORS
+
 import coma2.constants as c
 
 
 # init Flask app and SQLAlchemy database object
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = c.DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
