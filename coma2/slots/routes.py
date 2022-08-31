8b012bacbdf817d4eab76e5b8a3cf40dbf0e27a6
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-31 15:44:01 +0200

    move routes to modules

--- coma2/slots/routes.py
+++ coma2/slots/routes.py
@@ -1,6 +1,25 @@
-from flask import Blueprint
+import imp
+from flask import Blueprint, request, jsonify
+
+from coma2.settings import db, app
+from coma2.slots.models import Slot
 
 
 slots = Blueprint('slots', __name__)
 
 
+@app.route("/api/slots", methods=["PUT", "GET"])
+def handle_slot():
+    """Endpoint to get info about slots and to change single slots"""
+    if request.method == "PUT":
+        req = request.get_json()
+        slot = Slot.query.filter_by(id=req["id"]).first()
+        if "ingredient_id" in req.keys():
+            slot.ingredient_id = req["ingredient_id"]
+        if "amount_percentage" in req.keys():
+            slot.amount_percentage = req["amount_percentage"]
+        db.session.commit()
+        return jsonify(slot.serialize)
+    if request.method == "GET":
+        return jsonify([elem.serialize for elem in db.session.query(
+            Slot).order_by(Slot.id)])
