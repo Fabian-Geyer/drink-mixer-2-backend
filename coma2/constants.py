15317c0aef902bb3837e09655ed385539ca9d953
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2023-10-04 21:54:32 +1000

    format document

--- coma2/constants.py
+++ coma2/constants.py
@@ -1,5 +1,5 @@
 DEBUG = True
 DATABASE_URI = "sqlite:///coma2.db"
 
-SWAGGER_URL="/api/docs"
-SWAGGER_PATH="./coma2/docs/openapi.yml"
+SWAGGER_URL = "/api/docs"
+SWAGGER_PATH = "./coma2/docs/openapi.yml"
